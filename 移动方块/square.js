9d789f502275bdceed814dca842ca6f69492e71c
tyh <756467256@qq.com>
2019-12-19 20:38:33 +0800

    {YunHan}{fixed}:Fix problems with first move

--- 移动方块/square.js
+++ 移动方块/square.js
@@ -1,28 +1,30 @@
 var box = document.getElementsByClassName("box")[0];
-var move = document.getElementsByClassName("move")[0];
-var fixed = document.getElementsByClassName("fixed")[0];
+var moveBox = document.getElementsByClassName("move-box")[0];
+var fixedBox = document.getElementsByClassName("fixed-box")[0];
 
-move.onmousedown = function () {
+moveBox.onmousedown = function (e) {
+  var mouseX = e.clientX - box.offsetLeft - moveBox.offsetLeft;
+  var mouseY = e.clientY - box.offsetTop - moveBox.offsetTop;
   document.onmousemove = function (event) {
-    var moveX = event.clientX - box.offsetLeft;
-    var moveY = event.clientY - box.offsetTop;
-    if (moveX <= (box.offsetWidth - move.offsetWidth) 
+    var moveX = event.clientX - box.offsetLeft - mouseX;
+    var moveY = event.clientY - box.offsetTop - mouseY;
+    if (moveX <= (box.offsetWidth - moveBox.offsetWidth) 
         && moveX > 0) {
-      move.style.left = moveX + "px";
+      moveBox.style.left = moveX + "px";
     };
-    if (moveY <= (box.offsetHeight - move.offsetHeight) 
+    if (moveY <= (box.offsetHeight - moveBox.offsetHeight) 
         && moveY > 0) {
-      move.style.top = moveY + "px";
+      moveBox.style.top = moveY + "px";
     };
-    if ((moveX >= (fixed.offsetLeft - move.offsetWidth))
-        && (moveX <= (fixed.offsetLeft + fixed.offsetWidth))
-        && (moveY >= (fixed.offsetTop - move.offsetHeight))
-        && (moveY <= (fixed.offsetTop + fixed.offsetHeight))) {
-      fixed.style.backgroundColor = "blue";
+    if ((moveX >= (fixedBox.offsetLeft - moveBox.offsetWidth))
+        && (moveX <= (fixedBox.offsetLeft + fixedBox.offsetWidth))
+        && (moveY >= (fixedBox.offsetTop - moveBox.offsetHeight))
+        && (moveY <= (fixedBox.offsetTop + fixedBox.offsetHeight))) {
+      fixedBox.style.backgroundColor = "blue";
     }
   }
 }
 
-move.onmouseup = function () {
+document.onmouseup = function () {
   document.onmousemove = null;
 }
