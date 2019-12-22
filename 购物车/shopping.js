256bdf0fceb2e0599a68d8db335806b6907444dc
tyh <756467256@qq.com>
2019-12-22 22:49:46 +0800

    change the addEventLisenter

--- 购物车/shopping.js
+++ 购物车/shopping.js
@@ -49,6 +49,8 @@ var tr = document.getElementsByTagName("tr");
 var sum = document.getElementsByClassName("sum")[0];
 var countList = document.getElementsByClassName("count");
 var checkBox = document.getElementsByClassName("check-box");
+var totalPrice = 0;
+var countTotal = 0;
 
 //添加表格
 for (var i = 0; i < carProducts.length; i++) {
@@ -68,8 +70,8 @@ for (var i = 0; i < carProducts.length; i++) {
                  "<td class='item-sum'> " + carProducts[i].count * carProducts[i].price + "</td>" 
   tbody.insertBefore(tr, sumRow);
 }
-var totalPrice = 0;
-var countTotal = 0;
+
+//初始化总价
 for (var i = 0, len = carProducts.length; i < carProducts.length; i++) {
   if (checkBox[i].checked) {
     totalPrice += parseFloat(tbody.children[i+1].lastChild.innerText);
@@ -78,36 +80,32 @@ for (var i = 0, len = carProducts.length; i < carProducts.length; i++) {
   };
 sum.innerHTML = "共计" + countTotal + "件商品，" + totalPrice + "￥";
 
-//按加号增加数量
-function countAdd(e) {
-  var rowIndex = e.target.parentElement.parentElement.rowIndex;
-  countList[rowIndex - 1].children[1].innerText++;
-}
-
-//按减号减少数量
-function countCut(e) {
-  var rowIndex = e.target.parentElement.parentElement.rowIndex;
-  countList[rowIndex - 1].children[1].innerText--;
-}
-
-//按全选，没有选中的时候点击全选就全选，再点取消全选
-function selectAll(e) {
-  if (allSelect.checked) {
-    for (var i = 1, len = tbody.rows.length; i < len - 1; i++) {
-      tbody.children[i].firstChild.firstChild.checked = true;
-    }
-  }
-  else {
-    for (var i = 1, len = tbody.rows.length; i < len - 1; i++) {
-      tbody.children[i].firstChild.firstChild.checked = false;
-    }
-  }
-}
-
 //点击表格中的任何按键都会重新刷新总价
 function changeSum(e) {
   var rowIndex = e.target.parentElement.parentElement.rowIndex;
   var trCurrnt = tbody.children[rowIndex];  
+  //如果点的是加号，就让数量加一
+  if (e.target.className === "plus") {
+    countList[rowIndex - 1].children[1].innerText++;
+  };
+  //如果点的是减号，就让数量减一
+  if (e.target.className === "cut") {
+    countList[rowIndex - 1].children[1].innerText--;
+  };
+  //如果点的是全选，根据情况判断是全选还是取消全选
+  if (e.target.className === "all-select") {
+    if (e.target.checked) {
+      for (var i = 1, len = tbody.rows.length; i < len - 1; i++) {
+        tbody.children[i].firstChild.firstChild.checked = true;
+      }
+    }
+    else {
+      for (var i = 1, len = tbody.rows.length; i < len - 1; i++) {
+        tbody.children[i].firstChild.firstChild.checked = false;
+      }
+    }
+  }
+  //计算每件商品的总价和所有商品的总价
   if (rowIndex < tbody.rows.length-1) {
     trCurrnt.lastChild.innerText = trCurrnt.children[2].innerText * countList[rowIndex - 1].children[1].innerText;
   };
@@ -119,6 +117,7 @@ function changeSum(e) {
       countTotal += parseFloat(countList[i - 1].children[1].innerText);
     }
   };
+
   sum.innerHTML = "共计" + countTotal + "件商品，" + totalPrice + "￥";
   if (rowIndex < tbody.rows.length-1 
       && countList[rowIndex - 1].children[1].innerText === "0") {
@@ -128,17 +127,3 @@ function changeSum(e) {
 
 //给表格绑定点击事件，点击后刷新总价
 tbody.addEventListener("click", changeSum);
-
-//给加减号绑定点击事件，点击会改变数量
-var btnPlus = [];
-var btnCut = [];
-for (let id = 0, carProductsLen = carProducts.length; id < carProductsLen; id++) {
-  btnPlus[id] = document.getElementsByClassName("plus")[id];
-  btnPlus[id].addEventListener("click", countAdd);
-  btnCut[id] = document.getElementsByClassName("cut")[id];
-  btnCut[id].addEventListener("click", countCut);
-}
-
-//给全选键绑定点击事件
-var allSelect = document.getElementsByClassName("all-select")[0];
-allSelect.addEventListener("click", selectAll);
