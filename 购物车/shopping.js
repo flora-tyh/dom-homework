1d255501edb88d4c71ad1aa9ee71de7dab967f89
tyh <756467256@qq.com>
2019-12-22 23:56:16 +0800

    {YunHan}{fixed}:standardized code

--- 购物车/shopping.js
+++ 购物车/shopping.js
@@ -49,8 +49,60 @@ var tr = document.getElementsByTagName("tr");
 var sum = document.getElementsByClassName("sum")[0];
 var countList = document.getElementsByClassName("count");
 var checkBox = document.getElementsByClassName("check-box");
-var totalPrice = 0;
-var countTotal = 0;
+
+//计算总价函数，执行一次刷新一次总价
+function totalize () {
+  var totalPrice = 0;
+  var countTotal = 0;
+  for (var i = 0, len = tbody.rows.length; i < len - 2; i++) {
+    if (checkBox[i].checked) {
+      totalPrice += parseFloat(tbody.children[i+1].lastChild.innerText);
+      countTotal += parseFloat(countList[i].children[1].innerText);
+      }
+    };
+  sum.innerHTML = "共计" + countTotal + "件商品，" + totalPrice + "￥";
+};
+
+//全选函数，根据全选框的情况判定是全选还是去取消全选
+function allSelect(e) {
+  if (e.target.checked) {
+    for (var i = 1, len = tbody.rows.length; i < len - 1; i++) {
+      tbody.children[i].firstChild.firstChild.checked = true;
+    }
+  }
+  else {
+    for (var i = 1, len = tbody.rows.length; i < len - 1; i++) {
+      tbody.children[i].firstChild.firstChild.checked = false;
+    }
+  }
+}
+
+//改变某一物品数量和总价的函数
+function changeSum(e) {
+  var rowIndex = e.target.parentElement.parentElement.rowIndex;
+  var trCurrnt = tbody.children[rowIndex];  
+  //如果点的是加号，就让数量加一
+  if (e.target.className === "plus") {
+    countList[rowIndex - 1].children[1].innerText++;
+  };
+  //如果点的是减号，就让数量减一
+  if (e.target.className === "cut") {
+    countList[rowIndex - 1].children[1].innerText--;
+    if (rowIndex < tbody.rows.length-1 
+      && countList[rowIndex - 1].children[1].innerText === "0") {
+      tbody.deleteRow(rowIndex);
+    }; 
+  };
+  //如果点的是全选，执行全选函数
+  if (e.target.className === "all-select") {
+    allSelect(e);
+  };
+  //计算每件商品的总价和所有商品的总价
+  if (rowIndex < tbody.rows.length-1) {
+    trCurrnt.lastChild.innerText = trCurrnt.children[2].innerText * countList[rowIndex - 1].children[1].innerText;
+  };
+  totalize ();
+}
 
 //添加表格
 for (var i = 0; i < carProducts.length; i++) {
@@ -70,60 +122,7 @@ for (var i = 0; i < carProducts.length; i++) {
                  "<td class='item-sum'> " + carProducts[i].count * carProducts[i].price + "</td>" 
   tbody.insertBefore(tr, sumRow);
 }
-
 //初始化总价
-for (var i = 0, len = carProducts.length; i < carProducts.length; i++) {
-  if (checkBox[i].checked) {
-    totalPrice += parseFloat(tbody.children[i+1].lastChild.innerText);
-    countTotal += parseFloat(countList[i].children[1].innerText);
-    }
-  };
-sum.innerHTML = "共计" + countTotal + "件商品，" + totalPrice + "￥";
-
-//点击表格中的任何按键都会重新刷新总价
-function changeSum(e) {
-  var rowIndex = e.target.parentElement.parentElement.rowIndex;
-  var trCurrnt = tbody.children[rowIndex];  
-  //如果点的是加号，就让数量加一
-  if (e.target.className === "plus") {
-    countList[rowIndex - 1].children[1].innerText++;
-  };
-  //如果点的是减号，就让数量减一
-  if (e.target.className === "cut") {
-    countList[rowIndex - 1].children[1].innerText--;
-  };
-  //如果点的是全选，根据情况判断是全选还是取消全选
-  if (e.target.className === "all-select") {
-    if (e.target.checked) {
-      for (var i = 1, len = tbody.rows.length; i < len - 1; i++) {
-        tbody.children[i].firstChild.firstChild.checked = true;
-      }
-    }
-    else {
-      for (var i = 1, len = tbody.rows.length; i < len - 1; i++) {
-        tbody.children[i].firstChild.firstChild.checked = false;
-      }
-    }
-  }
-  //计算每件商品的总价和所有商品的总价
-  if (rowIndex < tbody.rows.length-1) {
-    trCurrnt.lastChild.innerText = trCurrnt.children[2].innerText * countList[rowIndex - 1].children[1].innerText;
-  };
-  totalPrice = 0;
-  countTotal = 0;
-  for (var i = 1, len = tbody.rows.length; i < len - 1; i++) {
-    if (tbody.children[i].firstChild.firstChild.checked) {
-      totalPrice += parseFloat(tbody.children[i].lastChild.innerText);
-      countTotal += parseFloat(countList[i - 1].children[1].innerText);
-    }
-  };
-
-  sum.innerHTML = "共计" + countTotal + "件商品，" + totalPrice + "￥";
-  if (rowIndex < tbody.rows.length-1 
-      && countList[rowIndex - 1].children[1].innerText === "0") {
-    tbody.deleteRow(rowIndex);
-  }; 
-}
-
+totalize ();
 //给表格绑定点击事件，点击后刷新总价
 tbody.addEventListener("click", changeSum);
